71963e34d94f63267beaf82409d5caaf56e8ff1b
Raphi W <ditshej@gmail.com>
2021-06-26 12:59:19 +0200

    Prime Factors Kata

---
+++ tests/PrimeFactorsTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\PrimeFactors;
+use PHPUnit\Framework\TestCase;
+
+class PrimeFactorsTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider factors
+     */
+    public function it_generates_prime_factors($number, $expected): void
+    {
+        $factors = new PrimeFactors;
+
+        self::assertEquals($expected, $factors->generate($number));
+    }
+
+    public function factors(): array
+    {
+        return [
+            [1, []],
+            [2, [2]],
+            [3, [3]],
+            [4, [2, 2]],
+            [5, [5]],
+            [6, [2, 3]],
+            [7, [7]],
+            [8, [2, 2, 2]],
+            [100, [2, 2, 5, 5]],
+            [999, [3, 3, 3, 37]],
+        ];
+    }
+
+
+}
